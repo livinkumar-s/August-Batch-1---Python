d9914dd40d3f6603cee8145c8106375a730f8bdc
niranjan122005 <niranjanv@thestackly.com>
2026-08-07 11:33:46 +0530

    Intro

---
+++ intro.py
@@ -0,0 +1,13 @@
+# these are printing statements to show a data in output
+# print(40)
+# print(65)
+# print(5)
+# print(40)
+# print(65)
+print(5)
+print(40)
+print(65)
+'''print(5)
+print(40)
+print(65)
+print(5)'''
