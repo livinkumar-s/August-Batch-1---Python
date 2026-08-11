04c4d6c46b892e608565cede8269f86a9d4d312a
niranjan122005 <niranjanv@thestackly.com>
2026-08-11 11:48:07 +0530

    Variables, Datatypes and Typecasting

---
+++ VariablesDatatypesTypecasting.py
@@ -0,0 +1,35 @@
+# a1=10 #creatiopn and assignment
+# print(a1)
+# a1=12 # re-assignment
+
+a=10
+# print(a)
+# print(type(a))
+
+b=10.1
+# print(a)
+# print(type(a))
+
+# print(a+b)
+
+# c='I said "Hi"'
+# print(c)
+# print(type(c))
+
+# str1="Hello"
+# str2=" "
+# str3="World!"
+# print(str1+str2+str3)
+# print(str1+a)
+
+name="Vijay"
+age=54
+
+#His name is Vijay. He is 54.
+
+# print("His name is "+name+". he is "+str(age)+".")
+
+# a=10
+# print(type(a))
+# b=str(600) #'600'
+# print(type(b))
