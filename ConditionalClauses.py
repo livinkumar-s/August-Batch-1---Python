0592d357139a031971d5d09cd42a8616ca40d92e
niranjan122005 <niranjanv@thestackly.com>
2026-08-18 11:33:11 +0530

    Conditional Clause Statements

---
+++ ConditionalClauses.py
@@ -0,0 +1,92 @@
+# print(10)
+# print(11)
+# print(12)
+# print(13)
+# print(14)
+
+# age=4
+
+# if age>=18:
+#     print("Can vote")
+# else:
+#     print("Cannot vote")
+
+# a=120
+
+# if a>55:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("---------------")
+# else:
+#     print("From else Block")
+
+# x=10
+
+# if x>10:
+#     print("From if")
+# elif x>10:
+#     print("From elif1")
+# elif x<10:
+#     print("From elif2")
+# elif x!=10:
+#     print("From elif3")
+
+# print(66)
+# a=input("Enter Your Name: ")
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+
+# inp=int(input("Enter a Number: ")) 
+# print(inp+100)
+# print(inp-100)
+
+# mark = int(input("Enter the mark: "))
+
+# if mark<0 or mark>100:
+#     print("Invalid marks")
+# elif mark >= 90:
+#     print("Grade: O")
+# elif mark >= 80:
+#     print("Grade: A+")
+# elif mark >= 70:
+#     print("Grade: A")
+# elif mark >= 60:
+#     print("Grade: B")
+# elif mark >= 50:
+#     print("Grade: C")
+# else:
+#     print("Grade: Fail")
+
+
+# a=int(input("Enter a number: "))
+
+# if a>10:
+#     if a>15:
+#         print("Very Large")
+#     else:
+#         print("Large")
+# else:
+#     if a<5:
+#         print("Very Small")
+#     else:
+#         print("Small")
+
+accType="current"
+balance=1001
+
+# if accType=="current":
+#     if balance>1000:
+#         print("Eligible")
+#     else:
+#         print("Not eligible")
+# else:
+#     print("Not Eligible")
+
+if accType=="current" and balance>1000:
+    print("Eligible")
+else:
+    print("Not Eligible")
