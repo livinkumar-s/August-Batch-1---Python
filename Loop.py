eb3c89c9930490ad3333f1414afa62a6061d1d6f
niranjan122005 <niranjanv@thestackly.com>
2026-08-20 11:55:10 +0530

    Loops - For and While

---
+++ Loop.py
@@ -0,0 +1,69 @@
+# i=0
+
+# while i<5:
+#     print(1)
+#     print(2)
+#     print(3)
+#     print("-------------")
+#     i+=1
+
+# No of execution --> 0
+# i --> 0
+
+# "Hello" # "e"
+
+# a="Hello"
+# print(a)
+
+# s1={34,43,23,1,-1}
+# print(s1)
+
+# str1="Hel"
+# print(str1[10])
+# print(len(str1))
+
+# For 
+# for i in range(100):
+#     print("Hello")
+
+# a=range(2,51,2)
+# print(list(a))
+
+# i=0
+# while i<3:
+#     print("Hello")
+#     print("Hi")
+#     print("Welcom")
+#     i+=1
+
+
+# for i in range(3):
+#     print("Hello")
+#     print("Hi")
+#     print("Welcom")
+    
+
+# while True:
+#     print("Hello")
+
+# for i in 
+
+
+# for i in range(1,6):
+#     if i==3:
+#         continue
+#     print(i)
+
+
+# print("Done")
+
+i=1
+while i<=10:
+    if i==5:
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+# No of iterattions --> 1,2,3,4,5.....10
+# i = 1,2,3,4,5,6
