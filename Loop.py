4b59d1d659cb8efc38c2d25f14e789a63f63e184
niranjan122005 <niranjanv@thestackly.com>
2026-08-24 12:07:59 +0530

    Nested Loops

--- Loop.py
+++ Loop.py
@@ -57,13 +57,61 @@
 
 # print("Done")
 
-i=1
-while i<=10:
-    if i==5:
-        i+=1
-        continue
-    print(i)
-    i+=1
+# i=1
+# while i<=10:
+#     if i==5:
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
 
 # No of iterattions --> 1,2,3,4,5.....10
 # i = 1,2,3,4,5,6
+
+# secret=33
+
+# while True:
+#     inp=int(input("Guess the number: "))
+#     if secret==inp:
+#         print("You won")
+#         break
+#     else:
+#         print("Try again!")
+
+# str1="Hello"
+
+# for i in str1:
+#     print(i)
+
+# i=0
+# while i<=4:
+#     print(str1[i])
+#     i+=1
+
+# for i in range(5):
+#     print("Hi")
+#     for j in range(3):
+#         print("Hello")
+
+len=int(input("Enter: "))
+
+
+for i in range(1,len+1): #i=1,2,3
+    for j in range(1,len+1): #j=1,2,3,4,5
+         if i==j or i+j==len+1:
+             print("* ",end="")
+         else:
+             print("  ",end="")
+    print()
+
+# print("Hello",12,54,67)
+# print("Hello\nHi")
+
+
+# str1="Hello\nwelcome"
+# print(str1)
+
+
+# print("Hello",end="123")
+# print("Balu")
+# print(12)
