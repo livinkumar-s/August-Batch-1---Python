704aa1663fca55af62551801a003aea92539ed18
niranjan122005 <niranjanv@thestackly.com>
2026-09-01 12:27:33 +0530

    Scopes, Membership, identity Operators

--- Functions.py
+++ Functions.py
@@ -70,3 +70,82 @@
 
 # print(45,end="")
 # print(54)
+
+# a=10 #global
+
+# def dummy():
+#     # a=10
+#     print(a)
+
+
+# dummy()
+# print(a)
+
+
+# def outer():
+#     def inner():
+#         a=20
+#     inner()
+#     print(a)
+
+# outer()
+
+
+# a=10
+
+# def func1():
+#     b=15
+#     def func2():
+#         c=4
+#         def func3():
+#             pass
+
+# a=10
+
+# def inc():
+#     global a 
+#     a+=10
+
+# inc()
+# inc()
+# inc()
+# print(a) #13
+
+
+# a=10
+# b=
+
+# print(id(a))
+# print(id(b))
+
+# a=5
+# b=a
+
+# a+=1
+
+# print(a)
+# print(b)
+
+# print(id(a))
+# print(id(b))
+
+# l1={1,2,3}
+# l2=l1
+
+# l1.add(33)
+
+# print(l1)
+# print(l2)
+# print(id(l1))
+# print(id(l2))
+
+# a=12
+# b=12
+
+# b+=1
+
+# print(a==b)
+# print(a is b)
+
+# print(1 in [1,2,3])
+# print("hell" not in "Hello world")
