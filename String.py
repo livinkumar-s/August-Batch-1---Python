f37e71cc255370ecda1df6656149a4355c956fe9
niranjan122005 <niranjanv@thestackly.com>
2026-09-03 11:20:06 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,62 @@
+# print("Hi"*3)
+
+# str1="Hello"
+# print(id(str1))
+# str1+=" World"
+# print(id(str1))
+# print(str1)
+
+# a='''Hello
+# Hi
+# Weolcome'''
+
+# print(a)
+
+# a="hello world"
+# print(a[:4])
+# a.upper()
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print(a.capitalize())
+# print(a.find("race"))
+# print(a.count("l"))
+
+# str1="I would like to code"
+
+# lis1=["Hello","world","welcome"]
+
+# print("__".join(lis1))
+
+#["I","would"...]
+
+# print(str1.split("o"))
+
+# a=[1,2,3]
+# a.append(5)
+# print(a)
+
+# print("12,123".isdigit())
+# print("helloworld".isalpha())
+# print("hello world".startswith("H"))
+# print("hello world".endswith("H"))
+
+# name="Vijay"
+# age=54
+
+#His name is Vijay. He is 54.
+
+# print("His name is "+name+". he is "+str(age)+".")
+# print(f"His name is {name}. he is {age}.")
+
+
+# a="Hello\nworld"
+# a="Hello\tworld"
+
+# a="',\",'''"
+
+# a="this is line breaker \\n"
+
+# this is line breaker \n 
+
+print(a)
